4d78664da34928c58582a7a2c1ea9ad2a9fdd21b
Sejal Chaudhary <sejalch2933@gmail.com>
2024-06-30 18:43:12 +0530

    sum of a and b

--- Variable_Datatype/Sum.java
+++ Variable_Datatype/Sum.java
@@ -1,6 +1,6 @@
 public class Sum {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 15;
         int b = 10;
         int sum = a+b;
         System.out.println(sum);
