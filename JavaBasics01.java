41faea45eecd21bfbe10f76dc998c2f62d9dacec
Sejal Chaudhary <sejalch2933@gmail.com>
2024-06-30 17:17:57 +0530

    java boilerplate with print output in 3 ways

---
+++ JavaBasics01.java
@@ -0,0 +1,11 @@
+public class JavaBasics01 {
+    public static void main(String[] args) {
+        System.out.print("start print");
+        System.out.println("Hello world 1!");
+        System.out.println("Hello world 2!");
+        System.out.print("Hello world next line!\n");
+
+
+
+    }
+}
