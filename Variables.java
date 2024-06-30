6380b5cf86c41b1f826dfc93fee9200e4d66c815
Sejal Chaudhary <sejalch2933@gmail.com>
2024-06-30 17:38:48 +0530

    variables

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Sejal Chaudhary";
+        System.out.println(name);
+
+        b = 25;
+        System.out.println(b);//25
+
+    }
+}
