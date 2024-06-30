07c8a2f784f51abebb7701a98e5ddc376ce4afad
Sejal Chaudhary <sejalch2933@gmail.com>
2024-06-30 18:20:59 +0530

    DataType with its size

---
+++ DataType.java
@@ -0,0 +1,25 @@
+import javax.swing.text.Style;
+
+public class DataType {
+    public static void main(String[] args) {
+        //primitive
+        byte b = 4;
+         System.out.println(b); //byte
+         char c = 'C';
+         System.out.println(c);//char
+         boolean is = false;
+         System.out.println(is);//boolean
+         float myNum = 5.75f;
+         System.out.println(myNum);
+         int num = 3;
+         System.out.println(num);
+         short n = 25;
+         System.out.println(n);
+         //long (to store large size number )
+         //double(to store large size decimal no.)
+
+
+
+         
+    }
+}
