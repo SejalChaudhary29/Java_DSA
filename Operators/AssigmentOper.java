fc9042f8706e211331a0863e72d863d6d6ce57b4
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-01 18:34:09 +0530

    Assignment Operator

---
+++ Operators/AssigmentOper.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class AssigmentOper {
+    public static void main(String[] args) {
+        /* Assignment Operator
+          
+        =, +=,-=,*=,/=
+
+         */
+        int a = 20;
+        a += 90;
+        System.out.println(a);//110
+
+        int b = 30;
+        b-= 10;
+        System.out.println(b); //20
+
+
+        // same for *= and /=
+    }
+}
