b25bacaf78e191800f8afc1bf53de2e81e2afb52
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-01 10:34:54 +0530

    side of square

---
+++ Assignment/Que2.java
@@ -0,0 +1,10 @@
+package Assignment;
+import java.util.*;
+public class Que2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int square  = a * a ;
+    System.out.println("side of square is " + square);
+  }  
+}
