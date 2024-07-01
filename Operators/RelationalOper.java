8d7ab6f2ee073991ed9ffaeeee83b8bdc09a87d0
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-01 11:54:15 +0530

    Relational operator

---
+++ Operators/RelationalOper.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class RelationalOper {
+    public static void main(String[] args) {
+        // relational operation give true or false output
+        int a = 10 ;
+        int b = 20;
+
+        System.out.println(a==b);//false
+        System.out.println(a<b);//true
+        System.out.println(a>b);//false
+        System.out.println(a<=b);//true
+        System.out.println(a>=b);//false
+        System.out.println(a!=b);//true
+    }
+}
