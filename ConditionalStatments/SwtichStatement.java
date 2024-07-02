0f68d72f326ea56a5d32a67957e36cd86e69da72
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-02 16:14:28 +0530

    Switch statement

---
+++ ConditionalStatments/SwtichStatement.java
@@ -0,0 +1,16 @@
+package ConditionalStatments;
+
+public class SwtichStatement {
+    public static void main(String[] args) {
+        int num = 2;
+        switch (num) {
+            case 1: System.out.println("samosa");
+            break;
+            case 2: System.out.println("Burger");
+            break;
+            case 3: System.out.println("Pizza");
+            break;
+            default:System.out.println("Empty");
+            }
+    }
+}
