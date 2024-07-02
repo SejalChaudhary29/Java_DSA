5a6e0906b9cbb07fdb4363b3ebd4893390dc06d5
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-02 08:50:56 +0530

    if else statement

---
+++ ConditionalStatments/ifElse.java
@@ -0,0 +1,14 @@
+package ConditionalStatments;
+
+public class ifElse {
+    public static void main(String[] args) {
+        int a = 22;
+        if (a >= 18) {
+            System.out.println("You are an Adult");
+        }
+        else{
+            System.out.println("You are not an Adult");
+        }
+    }
+    
+}
