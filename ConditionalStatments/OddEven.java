c5162f608a6adb4653ee0f929d6d72649cb1fa00
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-02 10:33:27 +0530

    odd even

---
+++ ConditionalStatments/OddEven.java
@@ -0,0 +1,18 @@
+package ConditionalStatments;
+import java.util.*;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        if (a%2==0) {
+            System.out.println("Even number " + a);
+            
+        } else {
+            System.out.println("Odd Number");
+            
+        }
+
+    }
+}
