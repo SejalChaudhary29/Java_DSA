a679c7a7bc95b07b168d87d5f1a6b91c62a66e6c
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-02 19:12:20 +0530

    While loops

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while ( count < 10) {
+            System.out.println("hello world" + count);
+            count++;
+        }
+        System.out.println("printed 10x time");
+    }
+}
