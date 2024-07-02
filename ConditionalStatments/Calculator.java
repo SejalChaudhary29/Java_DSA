85eec180a15bd7734b9563251b5ccf9ee7891d62
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-02 16:26:33 +0530

    Calculator using switch statement

--- ConditionalStatments/Calculator.java
+++ ConditionalStatments/Calculator.java
@@ -4,8 +4,11 @@ public class Calculator {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
+      System.out.print("enter a: ");
       int a = sc.nextInt();
+      System.out.print("enter b: ");
       int b = sc.nextInt();
+      System.out.print("enter operator: ");
       char operator = sc.next().charAt(0);
       switch (operator) {
         case '+': System.out.println(a+b);
