811b3adb9e1e966583d83e5217ef32d191a9e4f0
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-02 08:55:36 +0530

    print largest of two number

---
+++ ConditionalStatments/PrintLargest.java
@@ -0,0 +1,15 @@
+package ConditionalStatments;
+
+public class PrintLargest {
+    public static void main(String[] args) {
+        
+        int A = 1;
+        int B = 3; 
+    
+        if(A>B){
+            System.out.println("A is greated than B");
+    }   else {
+            System.out.println("B is greater than A");
+        }
+    }
+}
