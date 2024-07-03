73aaeb497057b37e79f1b9322e6494fd3fc438cc
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-03 12:47:24 +0530

    how many time Hello printed

---
+++ Assignment/assignment3/Que1.java
@@ -0,0 +1,12 @@
+package Assignment.assignment3;
+
+public class Que1 {
+    public static void main(String[] args) {
+        
+        for( int i=0; i<5; i++){
+            System.out.println("Hello"); // Hello prints 2 times
+            i+=2;
+        }
+    }
+}
+
