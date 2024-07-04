9a7a9d75273a8df72a98bf03e0f1ddf60efa4bc3
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-04 11:55:13 +0530

    que 4 printed multiple of n

---
+++ Assignment/assignment3/Que4.java
@@ -0,0 +1,13 @@
+package Assignment.assignment3;
+import java.util.*;
+public class Que4 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);    
+     int n = sc.nextInt();
+      
+     for(int i = 1; i<=10; i++ ){
+         System.out.println(n + "*" + i + "=" + n*i);
+        
+     }
+    }
+}
