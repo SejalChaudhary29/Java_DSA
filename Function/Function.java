964cdc9860d41c4f7239bec153220844554c0a0d
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-07 22:08:34 +0530

    Function and syntax

---
+++ Function/Function.java
@@ -0,0 +1,12 @@
+package Function;
+
+public class Function {
+    public static void printHello(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        
+        printHello();
+
+    }
+}
