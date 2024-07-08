5364bf7e41d2966ef5aee5d370c7f0f21904aead
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-08 12:58:07 +0530

    Function return

---
+++ Function/FunctionReturn.java
@@ -0,0 +1,14 @@
+package Function;
+//return keyword is must when the return type is int or other.  
+//incase of void it dosent need return statement as void means a empty 
+public class FunctionReturn {
+    public static int printHello() {
+        System.out.println("Hello sejal!!");
+        System.out.println("Hello sejal!!");
+        return 2;
+
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
