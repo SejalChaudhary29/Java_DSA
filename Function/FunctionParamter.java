0adf83a4f48588dcbd5d92eb2fc137753b16a49b
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-09 19:17:29 +0530

    adding comments

--- Function/FunctionParamter.java
+++ Function/FunctionParamter.java
@@ -1,7 +1,7 @@
 package Function;
 import java.util.*;
 public class FunctionParamter {
-    public static int calculateSum(int num1 , int num2) {
+    public static int calculateSum(int num1 , int num2) { // parameters 
         
         int sum = num1 + num2 ;
         return sum;
@@ -11,7 +11,7 @@ public class FunctionParamter {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = calculateSum(a, b);
+        int sum = calculateSum(a, b); //  arguements
         System.out.println("sum is "+ sum);
     }
 }
