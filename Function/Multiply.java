dcbc6e5845cf7f3d6a235d5d7ad2d633125b00f9
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-15 19:43:39 +0530

    Multiply a*b

---
+++ Function/Multiply.java
@@ -0,0 +1,13 @@
+package Function;
+
+public class Multiply {
+    public static int Multiply( int a , int b ) {
+        
+     int product = a*b;
+     return product;
+    }
+    public static void main(String[] args) {
+       System.out.println(Multiply(2, 3));
+        
+    }
+}
