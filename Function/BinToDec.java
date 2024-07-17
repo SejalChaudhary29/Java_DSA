5ca614526bbd9785cc9efbe656ff402ef5c248c7
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-17 19:59:03 +0530

    binary to decimal function

--- Function/BinToDec.java
+++ Function/BinToDec.java
@@ -6,7 +6,7 @@ public class BinToDec {
         int pow = 0;
         int decNum = 0;
 
-        while (binNum > 0) {
+        while(binNum > 0) {
 
             int lastDigit = binNum % 10;
             decNum = decNum + (lastDigit * (int) Math.pow(2, pow));
