c9c1490da11d11f4fc650b9f395fa0aae3241261
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-20 12:33:09 +0530

    Diamond pattern

--- Pattern2/DiamondPattern.java
+++ Pattern2/DiamondPattern.java
@@ -16,7 +16,7 @@ public class DiamondPattern {
             System.out.println();
 
         } // second half
-        for (int i = n - 1; i >= 1; i--) {
+        for (int i =n; i >= 1; i--) {
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
