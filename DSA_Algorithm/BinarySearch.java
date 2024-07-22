a4fbbd52fe6cca206ee5b14107d7474755a6eb94
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-22 11:32:13 +0530

    Binary Search

---
+++ DSA_Algorithm/BinarySearch.java
@@ -0,0 +1,36 @@
+package DSA_Algorithm;
+
+public class BinarySearch {
+
+    public static int binarySearch(int num[] , int key){
+
+        int start = 0 , end = num.length-1;
+
+        while ( start <=end) {
+            int mid = (start+end)/2;
+            if (num[mid] == key) {
+                return mid;
+                                                                                                                                                                                                                    
+                
+            }
+            if (num[mid] < key) { //right
+                 start = mid+ 1;
+                
+            }else{ //left
+                end = mid-1;
+
+            }
+            
+        }
+        return -1;
+
+
+    }
+    public static void main(String[] args) {
+        int num[] = {2 , 4 , 6 , 8 , 10};
+        int key = 10;
+        System.out.println(" Index for key is: "  +binarySearch(num, key)); //4
+    
+    }
+}
+ 
