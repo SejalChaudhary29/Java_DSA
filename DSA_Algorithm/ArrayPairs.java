041464ac84c63f838569c7b6cfefb9731b5fc2bb
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-22 18:43:48 +0530

    Arrays pair

---
+++ DSA_Algorithm/ArrayPairs.java
@@ -0,0 +1,20 @@
+package DSA_Algorithm;
+
+public class ArrayPairs {
+    public static void arrayPair(int num[]){
+ for ( int i = 0; i < num.length; i++) {
+    int curr = num[i];
+    for (int j = i+1; j < num.length; j++) {
+        System.out.print("(" + curr + "," + num[j] + ")");
+    }
+ System.out.println();
+ }
+
+
+    }
+    public static void main(String[] args) {
+        int num[] = {2 , 4 , 6 ,8 , 10};
+        arrayPair(num);
+        
+    }
+}
