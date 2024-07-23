f649e27cf0e8cfa81ec4f2923477aa35bf67fef1
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-23 10:45:47 +0530

    Printing Subarray

--- DSA_Algorithm/SubArrays.java
+++ DSA_Algorithm/SubArrays.java
@@ -2,15 +2,17 @@ package DSA_Algorithm;
 
 public class SubArrays {
     public static void subArray(int num[]) {
+        int ts = 0;
         for (int i = 0; i < num.length; i++) { //start
             for (int j = i; j < num.length; j++) { //end
                 for (int k = i; k <= j; k++) { //start to end
                     System.out.print(num[k] + " ");
                 }
                 System.out.println(); // Move to next line after printing each subarray
+                ts++;
             }
         }
-        System.out.println();
+        System.out.println("the total space is :" + ts);
     }
 
     public static void main(String[] args) {
