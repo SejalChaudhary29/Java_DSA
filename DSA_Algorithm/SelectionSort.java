456a131d1f7a14204c233ecc853b5a0af2c2b9cf
Sejal Chaudhary <sejalch2933@gmail.com>
2024-07-31 20:41:46 +0530

    selection sort

---
+++ DSA_Algorithm/SelectionSort.java
@@ -0,0 +1,28 @@
+package DSA_Algorithm;
+
+public class SelectionSort {
+    public static void Selection_Sort(int arr[]){
+
+             for (int i = 0; i < arr.length-1; i++) {//turns
+                int minPosition = i;
+                for (int j = i+1; j < arr.length; j++) {
+                    if(arr[minPosition] > arr[j]){
+                        minPosition = j;
+                    }
+                }
+                
+                int temp = arr[minPosition];
+                arr[minPosition] = arr[i];
+                arr[i] = temp;
+             }
+            
+    }
+    public static void main(String[] args) {
+        int arr[] = {5 , 4 , 1, 3 ,2 };
+       Selection_Sort(arr);
+       for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+
+    }
+}
