de2012ba03bd7c8c08ed3cbef739d05e94365579
Sejal Chaudhary <sejalch2933@gmail.com>
2024-08-03 19:12:20 +0530

    bubblesort

--- DSA_Algorithm/BubbleSort.java
+++ DSA_Algorithm/BubbleSort.java
@@ -2,7 +2,7 @@ package DSA_Algorithm;
 
 public class BubbleSort {
     public static int bubbleSort(int arr[]) {
-        int swapCount = 0;  // Initialize swap counter
+        int swapCount = 0; // Initialize swap counter
 
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
@@ -17,30 +17,15 @@ public class BubbleSort {
                 }
             }
         }
-        return swapCount;  // Return the total number of swaps
+        return swapCount; // Return the total number of swaps
     }
 
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
-
-        // Print array before sorting
-        System.out.print("Before sorting: ");
+         bubbleSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+           
         }
-        System.out.println();
-
-        // Sort the array and get the number of swaps
-        int swaps = bubbleSort(arr);
-
-        // Print array after sorting
-        System.out.print("After sorting: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
-        // Print the number of swaps
-        System.out.println("Total swaps: " + swaps);
     }
 }
