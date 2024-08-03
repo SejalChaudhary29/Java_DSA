052353711d9d6cd3dbc7f15aa42ba9122c1ba068
Sejal Chaudhary <sejalch2933@gmail.com>
2024-08-03 20:23:54 +0530

    Bubble sort with its best and worst complexity

--- DSA_Algorithm/BubbleSort.java
+++ DSA_Algorithm/BubbleSort.java
@@ -1,24 +1,29 @@
 package DSA_Algorithm;
 
 public class BubbleSort {
-    // TC = O(n^2)
-    public static int bubbleSort(int arr[]) {
-        int swapCount = 0; // Initialize swap counter
-
+    // TC = O(n^2) worst complexity 
+    // for  optimization we can add checks of didSwap O(n)
+    public static void bubbleSort(int arr[]) {
+        
         for (int i = 0; i < arr.length - 1; i++) {
+            int didSwap = 0; // Initialize swap counter
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap elements
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    didSwap = 1;
 
-                    // Increment swap counter
-                    swapCount++;
+                    
                 }
             }
+            if (didSwap==0) { // check for optimization O(n)
+                break;
+            }
+            System.out.println("loop runs");
         }
-        return swapCount; // Return the total number of swaps
+        
     }
 
     public static void main(String[] args) {
