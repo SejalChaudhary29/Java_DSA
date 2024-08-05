6883c92cbb55733d83c9f1fc4f83f1d010c5167e
Sejal Chaudhary <sejalch2933@gmail.com>
2024-08-05 14:11:05 +0530

    Bubble sort in descending order

--- Assignment/SortingAssignment/BubbleSort.java
+++ Assignment/SortingAssignment/BubbleSort.java
@@ -1,25 +1,23 @@
 package Assignment.SortingAssignment;
 
 public class BubbleSort {
-    public static void BubbleSort(int arr[]){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-1-i; j++) {
-                if (arr[j] < arr[j+1]) { // descending order
+    public static void BubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) { // descending order
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] =temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-                
+
             }
-            
+
         }
 
-
-
-
     }
+
     public static void main(String[] args) {
-        int arr[] = { 3 , 6 , 2 ,1, 8, 7 , 4 , 5 , 3 , 1};
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
         BubbleSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
