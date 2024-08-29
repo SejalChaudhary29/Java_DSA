978bfa14bd05fd93c4ece3590be19a7e9a1d3136
Sejal Chaudhary <sejalch2933@gmail.com>
2024-08-29 18:49:46 +0530

    String Converted toUpperCase

--- Strings/StringConvertToUpperCase.java
+++ Strings/StringConvertToUpperCase.java
@@ -2,8 +2,8 @@ package Strings;
 
 public class StringConvertToUpperCase {
 
+    // Time complexity=>O(n)
     public static String toUpperCase(String str){
-
     StringBuilder sb = new StringBuilder("");
         char ch = Character.toUpperCase(str.charAt(0));
         sb.append(ch);
