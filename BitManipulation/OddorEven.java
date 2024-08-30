8c45ab22e8447518d3f3d9625ff7b07d9a4bb021
Sejal Chaudhary <sejalch2933@gmail.com>
2024-08-30 20:20:43 +0530

    Odd or Even number using Bitwise operator

---
+++ BitManipulation/OddorEven.java
@@ -0,0 +1,18 @@
+package BitManipulation;
+import java.util.*;
+public class OddorEven {
+    public static void oddEven(int n ){
+        int Bit = 1;
+        if ((n & Bit) == 0) {
+            //even num
+            System.out.println("Even Number");
+        } else{
+            System.out.println("Odd Number");
+        }
+          }
+          public static void main(String[] args) {
+            oddEven(9);//odd
+            oddEven(2);//even
+            oddEven(19);//odd
+          }
+}
