aed0b01c1384051cde19d268533e72a743687cd6
Sejal Chaudhary <sejalch2933@gmail.com>
2024-09-01 11:42:32 +0530

    Get ith Bit (n& (1<<i))

---
+++ BitManipulation/GetIthBit.java
@@ -0,0 +1,17 @@
+package BitManipulation;
+
+public class GetIthBit {
+    public static int GetIthBit(int n, int i) {
+        int BitMask = 1 << i;
+
+        if ((n & BitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+System.out.println(GetIthBit(8 , 2));
+    }
+}
